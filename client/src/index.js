9acc4c902e94597287c16eb2d485739e418327f8
chyga2 <chyga2@yandex.ru>
2022-05-31 23:38:16 +0300

    почти готовый навбар

---
+++ client/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import {Footer} from './components/footer'
+import {Header} from './components/header';
+import { Navbar } from './components/navbar';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Header/>
+    <Navbar/>
+    <Footer />
+    </BrowserRouter>
+  </React.StrictMode>
+);
