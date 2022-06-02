a9f2d590b4af8e6496c70101933d4d2511b7b090
Dokolena <oweazzy@gmail.com>
2022-06-03 00:20:34 +0300

    controllers

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -1,9 +1,84 @@
-const Router = require('express')
-const router = Router()
 const ApiError = require('../error/apiError')
+const pool = require('./../db')
 
-class NewsController {
-
+const getAllNews = async (req, res) => {
+    try {
+        const allNews = await pool.query(
+            "SELECT * FROM news ORDER BY news_id")
+        res.json(allNews.rows)
+    } catch (err) {
+        console.error(err.message)
+    }
 }
 
-module.exports = new NewsController()
+const getSingleNews = async (req, res) => {
+    try {
+        const {
+            id
+        } = req.params
+        const singleNews = await pool.query(
+            "SELECT * FROM news WHERE news_id = $1", [
+                id
+            ]);
+        res.json(singleNews.rows[0])
+    } catch (err) {
+        console.error(err.message)
+    }
+}
+
+const createNews = async (req, res) => {
+    try {
+        const {
+            title,
+            body
+        } = req.body
+        const createNews = await pool.query(
+            "INSERT INTO news (title, body) VALUES($1, $2) RETURNING *",
+            [title, body])
+        res.json(createNews.rows)
+    } catch (err) {
+        console.error(err.message)
+    }
+}
+
+const deleteNews = async (req, res) => {
+    try {
+        const {
+            id
+        } = req.params
+        const deleteNews = await pool.query(
+            "DELETE FROM news WHERE news_id = $1", [
+                id
+            ]);
+        res.json(`Новость №${id} была удалена.`)
+    } catch (err) {
+        console.error(err.message)
+    }
+}
+
+const updateNews = async (req, res) => {
+    try {
+        const {
+            id
+        } = req.params
+        const {
+            title,
+            body
+        } = req.body
+        const updateNews = await pool.query(
+            "UPDATE news SET title = $1, body = $2 WHERE news_id = $3", [
+                title, body, id
+            ])
+        res.json(`Новость №${id} была обновлена`)
+    } catch (err) {
+        console.error(err.message)
+    }
+}
+
+module.exports = {
+    getAllNews,
+    getSingleNews,
+    createNews,
+    deleteNews,
+    updateNews,
+}
