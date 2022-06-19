f019d6f6e1360293e5b0a11206aa090794b5e29d
Dokolena <oweazzy@gmail.com>
2022-06-19 05:22:28 +0300

    some fixes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,25 +4,24 @@ const bcrypt = require('bcrypt')
 require('dotenv').config()
 const jwtGenerator = require('../utils/jwtGenerator')
 
-
-
 const getAllUsers = async (req, res) => {
     try {
-        const allUsers = await pool.query
-            ("SELECT * FROM users ORDER BY user_id")
+        const allUsers = await pool.query(
+            "SELECT * FROM users ORDER BY user_id"
+            )
         res.json(allUsers.rows)
     } catch (err) {
         console.error(err.message)
     }
 }
 
-
 const userLogin = async (req, res) => {
-    const { username, password } = req.body;
+    const {username, password} = req.body;
     try {
-        const user = await pool.query
-            ("SELECT * FROM users WHERE user_name = $1",
-                [username]);
+        const user = await pool.query(
+            "SELECT * FROM users WHERE user_name = $1",[
+                username
+            ]);
         if (user.rows.length === 0) {
             return res.status(401).json("Пользователь не найден");
         }
@@ -31,7 +30,7 @@ const userLogin = async (req, res) => {
             return res.status(401).json("Неправильный пароль");
         }
         const token = jwtGenerator(user.rows[0].user_id, username, user.rows[0].isadmin)
-        return res.json({ token })
+        return res.json({token})
 
     } catch (err) {
         console.error(err.message);
