0cd47210bc2826d898086f6c886e75df2af033f1
Dokolena <oweazzy@gmail.com>
2022-06-22 05:45:06 +0300

    vip

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -1,60 +1,57 @@
-const ApiError = require('../error/apiError')
-const pool = require('./../db')
+const ApiError = require('../error/apiError');
+const pool = require('../db');
 
 const getAllNews = async (req, res) => {
-    try {
-        const allNews = await pool.query(
-          `SELECT * FROM news ORDER BY news_id`
-          )
-        res.json(allNews.rows)
-    } catch (err) {
-        console.error(err.message)
-    }
-}
+  try {
+    const allNews = await pool.query(
+      'SELECT * FROM news ORDER BY news_id',
+    );
+    res.json(allNews.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
 
 const getOneNews = async (req, res) => {
-    try {
-        const {id} = req.params
-        const singleNews = await pool.query(
-          `SELECT * FROM news WHERE news_id = $1`,[
-                id
-              ])
-        res.json(singleNews.rows[0])
-    } catch (err) {
-        console.error(err.message)
-    }
-}
+  try {
+    const {id} = req.params;
+    const singleNews = await pool.query('SELECT * FROM news WHERE news_id = $1', [
+      id,
+    ]);
+    res.json(singleNews.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
 
 const createNews = async (req, res) => {
-    try {
-        const {title, body} = req.body
-        const createNews = await pool.query(
-          `INSERT INTO news (title, body) VALUES($1, $2) RETURNING *`,[
-            title, body
-        ])
-        res.json(createNews.rows)
-    } catch (err) {
-        console.error(err.message)
-    }
-}
+  try {
+    const {title, body} = req.body;
+    const createNews = await pool.query('INSERT INTO news (title, body) VALUES($1, $2) RETURNING *', [
+      title, body,
+    ]);
+    res.json(createNews.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
 
 const updateNews = async (req, res) => {
-    try {
-        const {id} = req.params
-        const {title, body} = req.body
-        await pool.query(
-          `UPDATE news SET title = $1, body = $2 WHERE news_id = $3`,[
-                title, body, id
-            ])
-        res.json(`Новость №${id} была обновлена`)
-    } catch (err) {
-        console.error(err.message)
-    }
-}
+  try {
+    const {id} = req.params;
+    const {title, body} = req.body;
+    await pool.query('UPDATE news SET title = $1, body = $2 WHERE news_id = $3', [
+      title, body, id,
+    ]);
+    res.json(`Новость №${id} была обновлена`);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
 
 module.exports = {
-    getAllNews,
-    getOneNews,
-    createNews,
-    updateNews,
-}
+  getAllNews,
+  getOneNews,
+  createNews,
+  updateNews,
+};
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,14 +1,15 @@
-const Router = require('express')
-const router = Router()
-const authMiddleware = require('../middleware/authMiddleware')
+const Router = require('express');
+
+const router = Router();
+const authMiddleware = require('../middleware/authMiddleware');
 const {
-    userCheck,
-    getAllUsers,
-    userLogin,
-} = require('../controllers/userController')
+  userCheck,
+  getAllUsers,
+  userLogin,
+} = require('../controllers/userController');
 
-router.get('/', getAllUsers)
-router.post('/login', userLogin)
-router.get('/auth', authMiddleware, userCheck)
+router.get('/', getAllUsers);
+router.post('/login', userLogin);
+router.get('/auth', authMiddleware, userCheck);
 
-module.exports = router
+module.exports = router;
