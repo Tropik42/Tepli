2a0802873976193254f82f02bf3211cd6135fda1
Dokolena <oweazzy@gmail.com>
2022-10-12 08:08:32 +0300

    надо вынести всю писанину в отдельный компонент

--- client/src/pages/News.js
+++ client/src/pages/News.js
@@ -1,92 +1,56 @@
 import React, {useEffect, useState} from "react"
+import {NewsList} from "../components/NewsList";
+
 const axios = require('axios')
 const News = () => {
-  const [title, setTitle] = useState("")
-  const [body, setBody] = useState("")
-  const [allnews, setNews] = useState([])
+    const [title, setTitle] = useState("")
+    const [body, setBody] = useState("")
 
-  async function getNews() {
-    const response = await axios.get('http://localhost:5000/api/v1/news')
-    setNews(response.data)
-    console.log(response)
-  }
-  useEffect(() => {
-      getNews();
-    }, []);
-
-  const onSubmitForm = async e => {
-    e.preventDefault()
-    try {
-      const postNews = {title, body}
-      const result = await axios.post('http://localhost:5000/api/v1/news', postNews)
-      console.log(result)
-    } catch (e) {
-      console.log(e.message())
+    const onSubmitForm = async e => {
+        e.preventDefault()
+        try {
+            const postNews = {title, body}
+            const result = await axios.post('http://localhost:5000/api/v1/news', postNews)
+            console.log(result)
+        } catch (e) {
+            console.log(e.message())
+        }
     }
-  }
-  console.log(allnews)
 
-  return (
-      <React.Fragment>
+    return (
+        <React.Fragment>
 
-        <form className={"d-flex mt-2"} onSubmit={onSubmitForm}>
-          <div className={"mb-3 d-flex"}>
-            <label className={"text-danger"}>Заголовок</label>
-            <input type={"text"}
-                   className={"form-control"}
-                   value={title}
-                   onChange={e => setTitle(e.target.value)}
-                   id="exampleFormControlInput1"/>
-          </div>
-          <div className={"mb-3"}>
-            <label className={"text-danger"}>Новость</label>
-            <textarea className={"form-control"}
-                      id="exampleFormControlTextarea1"
-                      value={body}
-                      onChange={e => setBody(e.target.value)}
-                      rows="3">
-                    </textarea>
-          </div>
-
-
-          <div className={"mb-3"}>
-            <label className={"text-danger"}>Изображение</label>
-            <input className={"form-control"} type={"file"} id={"formFile"}/>
-          </div>
-          <hr className={"invisible"}/>
-          <button className="btn btn-success float">Add</button>
-        </form>
-
-
-        {allnews.slice(0).reverse().map(news => (
-        <div>
-          <div className="wrapper">
-            <div className="container">
-              <div className="col-lg-12">
-                <div className="row news">
-
-                      <h2 className="h2_header">{news.title}</h2>
-
-                  <hr/>
-                  <div className="col-lg-3 col-md-2 text-center">
-                    <img className="img-thumbnail img-responsive pull-left" src="img/lumb2.jpg"
-                         alt="Безумный Макс"/>
-                  </div>
-                  <div className="col-lg-9 col-md-10">
-                        <p className="text-justify">{news.body}</p>
-                  </div>
-                  <div className="col-lg-12">
-                    <a href="" className="btn btn-lg btn-primary pull-right">Подробнее</a>
-                  </div>
+            <form className={"d-flex mt-2"} onSubmit={onSubmitForm}>
+                <div className={"mb-3 d-flex"}>
+                    <label className={"text-danger"}>Заголовок</label>
+                    <input type={"text"}
+                           className={"form-control"}
+                           value={title}
+                           onChange={e => setTitle(e.target.value)}
+                           id="exampleFormControlInput1"/>
                 </div>
-                <div className="margin-5"></div>
-              </div>
-            </div>
-          </div>
+                <div className={"mb-3"}>
+                    <label className={"text-danger"}>Новость</label>
+                    <textarea className={"form-control"}
+                              id="exampleFormControlTextarea1"
+                              value={body}
+                              onChange={e => setBody(e.target.value)}
+                              rows="3">
+                    </textarea>
+                </div>
 
-        </div>
-        ))}
-      </React.Fragment>
-  )
+                <div className={"mb-3"}>
+                    <label className={"text-danger"}>Изображение</label>
+                    <input className={"form-control"} type={"file"} id={"formFile"}/>
+                </div>
+                <hr className={"invisible"}/>
+                <button className="btn btn-success float">Add</button>
+            </form>
+
+            <NewsList/>
+
+        </React.Fragment>
+    )
 }
-export  {News}
+export {News}
+
