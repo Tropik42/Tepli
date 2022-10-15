b578c10e0f86887188f0a572f6e50442f55a7b74
Dokolena <oweazzy@gmail.com>
2022-10-15 08:39:17 +0300

    75% done

---
+++ client/src/components/SideBarComponent.js
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from "react"
+import axios from "axios";
+
+const SideBarComponent = () => {
+    const [allnews, setNews] = useState([])
+
+    async function getNews() {
+        await axios.get('http://localhost:5000/api/v1/news')
+            .then(response => setNews(response.data.slice(-5)))
+    }
+
+    useEffect(() => {
+        getNews();
+    }, []);
+
+    return (
+        <React.Fragment>
+            {(allnews.slice(0).reverse().map(news => (
+                        <div className="wrapper">
+                            <a href="#"><h4>{news.title}</h4></a>
+                            <p id="dateSpan">{(news.newsDate).slice(0, 10)}</p>
+                            <p className="text-justify">{news.body}</p>
+                            <hr/>
+                        </div>
+            )))}
+        </React.Fragment>
+    );
+}
+export {SideBarComponent};
