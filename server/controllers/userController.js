2a1b6302fbb0849b8d2a11331d7d7d910fff4c76
chyga2 <chyga2@yandex.ru>
2022-11-09 18:53:43 +0400

    registration is in order

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,13 +3,12 @@ const pool = require('../db');
 require('dotenv').config();
 const jwtGenerator = require('../utils/jwtGenerator');
 const queries = require('../queries/users');
-const jwtRegistration = require('../utils/jwtGenerator')
-
+const jwtRegistration = require('../utils/jwtGenerator');
 
 const getAllUsers = async (req, res) => {
   try {
     const allUsers = await pool.query(
-    queries.getAllUsers,
+      queries.getAllUsers,
     );
     res.json(allUsers.rows);
   } catch (err) {
@@ -20,34 +19,32 @@ const userRegistration = async (req, res) => {
   const {username, password} = req.body;
   try {
     const user = await pool.query(queries.userLogin, [username]);
-    if (user.rows.length > 0) {
-      return res.status(401).json("пользователь существует!");
+    if (user.rows.length) {
+      return res.status(401).json('пользователь существует!');
     }
     const salt = await bcrypt.genSalt(5);
     const bcryptPassword = await bcrypt.hash(password, salt);
-    let newUser = await pool.query(
+    const {newUser} = await pool.query(
       queries.createUser,
-      [username, bcryptPassword]
+      [username, bcryptPassword],
     );
-
     const jwtToken = jwtRegistration(newUser.rows[0].user_id);
-
     return res.json({jwtToken});
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("ошибка сервера");
+    return res.status(500).send('ошибка сервера');
   }
 };
 
 const userLogin = async (req, res) => {
   const {username} = req.body;
   try {
-    const user = await pool.query(queries.userLogin,[
-      username
+    const user = await pool.query(queries.userLogin, [
+      username,
     ]);
-    if (user.rows.length === 0) {
+    if (!user.rows.length) {
       return res.status(401).json('Пользователь не найден');
-    };
+    }
     const {userId, isAdmin, userPassword} = user.rows[0];
     const validPassword = await bcrypt.compare(req.body.password, userPassword) || 'adminpassword';
     if (!validPassword) return res.status(400).send('неверный пароль');
@@ -56,6 +53,7 @@ const userLogin = async (req, res) => {
     return res.json({token});
   } catch (err) {
     console.error(err.message);
+    return res.status(500).send('ошибка сервера');
   }
 };
 
@@ -68,5 +66,5 @@ module.exports = {
   userCheck,
   getAllUsers,
   userLogin,
-  userRegistration
+  userRegistration,
 };
