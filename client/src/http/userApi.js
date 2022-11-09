81c94936d2074547839618cf66be5029f84d7ab7
chyga2 <chyga2@yandex.ru>
2022-11-09 17:53:26 +0400

    code is in order

--- client/src/http/userApi.js
+++ client/src/http/userApi.js
@@ -5,10 +5,10 @@ import jwt_decode from 'jwt-decode'
 
 export const login = async (username, password,err) => {
     try{
-        const {data} = await $host.post('http://localhost:5000/api/v1/user/login', {username, password})
-        localStorage.setItem('token', data.token)
-        console.log(data.token);
-        return jwt_decode(data.token)}
+        const {data: {token}} = await $host.post('http://localhost:5000/api/v1/user/login', {username, password})
+        localStorage.setItem('token', token)
+        console.log('token', token);
+        return jwt_decode(token)}
     catch (err) {
         console.error(err.message)
     }
