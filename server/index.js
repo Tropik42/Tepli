af849bd4127ded98ca7050e3c74e1b86b13e8107
chyga2 <chyga2@yandex.ru>
2022-11-25 19:04:43 +0400

    insert image - working

--- server/index.js
+++ server/index.js
@@ -4,11 +4,13 @@ const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload());
 app.use('/api/v1', router);
 app.use(errorHandler);
