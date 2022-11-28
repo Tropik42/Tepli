a19c7b94e9c6092f6e9c7c37e684c14418ae3527
Tropik42 <62752057+Tropik42@users.noreply.github.com>
2022-11-28 13:19:17 +0400

    Update RequireAuth.js

--- client/src/hoc/RequireAuth.js
+++ client/src/hoc/RequireAuth.js
@@ -1,13 +1,13 @@
 import {useLocation, Navigate} from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "./AuthProvider"
+import {useContext} from "react";
+import {AuthContext} from "./AuthProvider"
 import {observer} from "mobx-react-lite"
 
 const RequireAuth = observer(({children}) => {
     const location = useLocation()
     const {user} = useContext(AuthContext)
     console.log(user)
-    if(!user.isAuth){
+    if (!user.isAuth) {
         return <Navigate to='/Auth' state={{from: location}}/>
     }
     return children
