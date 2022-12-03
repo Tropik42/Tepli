91116f4cd150be7d6bdc1171f0dfd208beba5e0e
Denis <oweazzy@gmail.com>
2022-12-03 06:11:37 +0300

    test

--- client/src/components/EditNews.js
+++ client/src/components/EditNews.js
@@ -1,73 +1,111 @@
-import React from "react";
-import instance from "../axios/axiosController";
+import React from 'react';
+import {Container, Button} from 'react-bootstrap';
+import instance from '../axios/axiosController';
 
 class EditNews extends React.Component {
     state = {
         newsId: '',
         newsTitle: this.props.news.newsTitle,
-        newsBody: this.props.news.newsBody
-    }
+        newsBody: this.props.news.newsBody,
+    };
+
     handleChangeTitle = (e) => {
-        this.setState({newsTitle: e.target.value})
-    }
+        this.setState({newsTitle: e.target.value});
+    };
+
     handleChangeBody = (e) => {
-        this.setState({newsBody: e.target.value})
-    }
+        this.setState({newsBody: e.target.value});
+    };
+
     handleUpdate = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         instance.put(`/news/${this.state.newsId}`, {
-            "title": this.state.newsTitle,
-            "body": this.state.newsBody
+            title: this.state.newsTitle,
+            body: this.state.newsBody,
         })
-            .then(res => {
-                console.log(res)
-            })
-    }
+            .then((res) => {
+                console.log(res);
+            });
+    };
 
     render() {
         if (this.state.newsId == '') {
             this.setState({
                 newsId: this.props.news.newsId,
                 newsTitle: this.props.news.newsTitle,
-                newsBody: this.props.news.newsBody
-            })
+                newsBody: this.props.news.newsBody,
+            });
         }
         return (
-            <div className={"container"} >
+            <Container>
 
-                <button type={"button"} className={"btn btn-info btn-lg"} data-toggle={"modal"} data-target={`#id${this.props.news.newsId}`}>
-                    Изменить
-                </button>
-
-                <div className={"modal fade"} id={`id${this.props.news.newsId}`} role={"dialog"}>
-                    <div className={"modal-dialog"}>
-                    <div className={"modal-content"} >
-                        <div className={"modal-header"}>
-                            <h4 className={"modal-title"}>Че сюда написать? =)</h4>
-                            <button type={"button"} className={"close"} data-dismiss={"modal"}>&times;</button>
-                        </div>
-                        <div className={"modal-body"}>
-                            <div className={"mb-3"}>
-                                <label className={"col-form-label"} for={"title-name"}>Новый заголовок:</label>
-                                <input id={"title-name"} className={"form-control"} name={"title"}
-                                       value={this.state.newsTitle} onChange={(e => this.handleChangeTitle(e))}/>
-                            </div>
-                            <div className={"mb-3"}>
-                                <label className={"col-form-label"} htmlFor={"body-name"}>Изменить новость:</label>
-                                <textarea id={"body-name"} className={"form-control"} name={"body"}
-                                          value={this.state.newsBody} onChange={(e => this.handleChangeBody(e))}/>
-                            </div>
-                            <div className={"modal-footer"}>
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">
+                <div className="container">
+                    <Button
+                        type="button"
+                        className="btn btn-info btn-lg pull-right"
+                        data-toggle="modal"
+                        data-target={`#id${this.props.news.newsId}`}
+                    >
+                Изменить
+                    </Button>
+                    <div className="modal fade" id={`id${this.props.news.newsId}`} role="dialog">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h4 className="modal-title">Че сюда написать? =)</h4>
+                                    <Button
+                                        type="button"
+                                        className="close"
+                                        data-dismiss="modal"
+                                    >
+                                &times;
+                                    </Button>
+                                </div>
+                                <div className="modal-body">
+                                    <div className="mb-3">
+                                        <label className="col-form-label" htmlFor="title-name">Новый заголовок:</label>
+                                        <input
+                                            id="title-name"
+                                            className="form-control"
+                                            name="title"
+                                            value={this.state.newsTitle}
+                                            onChange={((e) => this.handleChangeTitle(e))}
+                                        />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label className="col-form-label" htmlFor="body-name">Изменить новость:</label>
+                                        <textarea
+                                            id="body-name"
+                                            className="form-control"
+                                            name="body"
+                                            value={this.state.newsBody}
+                                            onChange={((e) => this.handleChangeBody(e))}
+                                        />
+                                    </div>
+                                    <div className="modal-footer">
+                                        <Button
+                                            className="btn btn-secondary"
+                                            type="button"
+                                            data-dismiss="modal"
+                                        >
                                     Закрыть
-                                </button>
-                                <button onClick={(e) => this.handleUpdate(e)} type="button" className="btn btn-primary">
+                                        </Button>
+                                        <Button
+                                            className="btn btn-primary"
+                                            type="button"
+                                            onClick={(e) => this.handleUpdate(e)}
+                                        >
                                     Сохранить
-                                </button>
+                                        </Button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div> </div> </div> </div>
-        )
+                    </div>
+                </div>
+
+            </Container>
+        );
     }
 }
 
