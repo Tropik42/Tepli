d5a02f32e47d6d747fcc2a6750f1336fa2572f8c
chyga2 <chyga2@yandex.ru>
2023-01-24 19:49:58 +0300

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, {useContext, useState, useEffect} from 'react';
 import {Route, Routes} from 'react-router-dom';
 import {observer} from 'mobx-react-lite';
-import jwt_decode from 'jwt-decode';
 import {About} from './pages/About';
 import {Catalog} from './pages/Catalog';
 import {Contacts} from './pages/Contacts';
@@ -12,7 +11,6 @@ import {Auth} from './pages/Auth';
 import {Registration} from './pages/Registration';
 import {SingleNews} from './pages/SingleNews';
 import './App.css';
-import {RequireAuth} from './hoc/RequireAuth';
 import {AuthContext} from './hoc/AuthProvider';
 import {check} from './http/userApi';
 import {RequireMain} from './hoc/RequireMain';
@@ -20,13 +18,11 @@ import {RequireMain} from './hoc/RequireMain';
 const App = observer(() => {
     const {user} = useContext(AuthContext);
     const [loading, setLoading] = useState(true);
-    // eslint-disable-next-line max-len
-    const asd = jwt_decode(localStorage.getItem('token') || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjozLCJ1c2VybmFtZSI6ImNoeWdhMiIsImlzQWRtaW4iOmZhbHNlfSwiaWF0IjoxNjcxOTgzNDU1LCJleHAiOjE2NzE5OTQyNTV9.WrOyMosaVVLl2Ke6Zo4k_M2phFdsbyHrD7BaUVc06hM');
     useEffect(() => {
-        check().then((data) => {
+        check().then((token) => {
             user.setUser(true);
             user.setIsAuth(true);
-            user.setIsAdmin(asd.user.isAdmin);
+            user.setIsAdmin(token.user.isAdmin);
         }).finally(() => setLoading(false));
     }, []);
     if (loading) { return console.log('загрузка'); }
